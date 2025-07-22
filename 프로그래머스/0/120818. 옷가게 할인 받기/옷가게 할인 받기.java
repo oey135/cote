5f84255849f5d94a7bfceb11247702064f41f621
oey135 <b01080126412@gmail.com>
2025-07-22 09:11:58 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int price) {
+        if(price >= 500000)
+            return (int)price*80/100;
+        else if(price >= 300000)
+            return (int)price*90/100;
+        else if(price >= 100000)
+            return (int)price*95/100;
+        else
+            return price;
+    }
+}
