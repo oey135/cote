2b1838c81e82b2c8455e4c84f2a6b9628b8919df
oey135 <b01080126412@gmail.com>
2026-03-12 00:49:33 +0900

    [level 0] Title: 공 던지기, Time: 0.06 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 1;
+        
+        for(int i=0; i<k-1; i++) {
+            answer += 2;
+            if(answer > numbers.length) {
+                answer-=numbers.length;
+            }
+        }
+        return answer;
+    }
+}
