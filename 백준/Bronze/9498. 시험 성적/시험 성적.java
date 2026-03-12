563f08fd1c23253ca3177e36503f1407701cbd4f
oey135 <b01080126412@gmail.com>
2026-03-13 00:28:45 +0900

    [Bronze V] Title: 시험 성적, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -4,9 +4,8 @@ class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int score = sc.nextInt();
-        score /= 10;
-        
-        switch(score) {
+
+        switch(score/=10) {
             case 10:case 9: System.out.println("A"); break;
             case 8: System.out.println("B"); break;
             case 7: System.out.println("C"); break;
