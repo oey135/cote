5b629e511cb8e451c0531b83483eae7888194f05
oey135 <b01080126412@gmail.com>
2026-03-14 15:07:14 +0900

    [Bronze V] Title: 사분면 고르기, Time: 180 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -4,15 +4,15 @@ class Main{
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         int y = sc.nextInt();
-        int quadrant = 0;
+        int quad = 0;
         if(x>0) {
-            if(y>0) quadrant = 1;
-            else quadrant = 4;
+            if(y>0) quad = 1;
+            else quad = 4;
         }
         else {
-            if(y>0) quadrant = 2;
-            else quadrant = 3;
+            if(y>0) quad = 2;
+            else quad = 3;
         }
-        System.out.print(quadrant);
+        System.out.print(quad);
     }
 }
