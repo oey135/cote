6c7e23d829bfbc703d22f02165f5d280adb3d3e0
oey135 <b01080126412@gmail.com>
2026-03-15 23:38:31 +0900

    [Bronze III] Title: 오븐 시계, Time: 188 ms, Memory: 18404 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int need = sc.nextInt();
+
+        m += need;
+        while(m>59) {
+            m = m-60;
+            h++;
+        }
+        while(h>23) {
+            h = h-24;
+        }
+        System.out.print(h+" "+m);
+    }
+}
