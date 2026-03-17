6109ebdd04946483b870126e4519fee2dd342737
oey135 <b01080126412@gmail.com>
2026-03-17 13:25:34 +0900

    [level 0] Title: OX퀴즈, Time: 0.20 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -1,25 +1,11 @@
+
 class Solution {
     public String[] solution(String[] quiz) {
-        String[] answer = new String[quiz.length];
-        
-        for(int i=0; i<quiz.length; i++) {
-            String[] temp = quiz[i].split(" ");
-            int a = Integer.valueOf(temp[0]);
-            int b = Integer.valueOf(temp[2]);
-            int result=0;
-            
-            switch(temp[1]) {
-                case "+": result = a+b; break;
-                case "-": result = a-b; break;
-                default: break;
-            }
-            if(result == Integer.valueOf(temp[4])) {
-                answer[i] = "O";
-            }
-            else {
-                answer[i] = "X";
-            }
+        for(int i=0; i<quiz.length; i++){
+            String[] text = quiz[i].split(" ");
+            int result = Integer.parseInt(text[0]) + ( Integer.parseInt(text[2]) * ( text[1].equals("+") ? 1:-1) );
+            quiz[i] = result == Integer.parseInt(text[4])? "O": "X";
         }
-        return answer;
+        return quiz;
     }
 }
