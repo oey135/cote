5859cc5e9d10468fea7c7e6abbd0b78bed67d560
oey135 <b01080126412@gmail.com>
2026-03-20 21:49:50 +0900

    [Bronze V] Title: A+B - 5, Time: 176 ms, Memory: 17876 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a*b!=0) System.out.println(a+b);
+            else break;
+        }
+    }
+}
