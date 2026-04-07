5c5acc4c9550abc2ab4b0d7f07e25922b8c0817d
oey135 <b01080126412@gmail.com>
2026-04-08 00:10:45 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,16 +1,10 @@
 class Solution {
     public int[] solution(int n) {
-        int an=0;
-        if(n%2==0)
-            an = n/2;
-        else
-            an = n/2+1;
+        int an = n%2==0 ? n/2 : n/2+1;
         int[] answer = new int[an];
-        an=-1;
+        an = 0;
         for(int i=1; i<=n; i++) {
-            if(i%2==0)
-                continue;
-            answer[++an] = i;
+            if(i%2==1) answer[an++] = i;
         }
         return answer;
     }
