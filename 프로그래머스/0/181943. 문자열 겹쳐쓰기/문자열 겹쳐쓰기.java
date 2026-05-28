ac264292c31755f31c74b9abf21ed03142dfa645
김서연 <b01080126412@gmail.com>
2026-05-29 01:53:23 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 63.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0,s);
+        answer = answer.concat(overwrite_string);
+        answer = answer.concat(my_string.substring(s+overwrite_string.length()));
+        return answer;
+    }
+}
