95bb54c793c4fe39a207241154b1cc0eb1666731
김서연 <b01080126412@gmail.com>
2026-06-11 16:40:31 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length+1];
+        
         for(int i=0; i<num_list.length; i++) {
             answer[i] = num_list[i];
         }
@@ -10,11 +11,7 @@ class Solution {
         int last = num_list[num_list.length-1];
         int sec = num_list[num_list.length-2];
         
-        if(sec < last) {
-            answer[answer.length-1] = last-sec;
-        } else {
-            answer[answer.length-1] = last*2;
-        }
+        answer[answer.length-1] = sec < last ? last-sec : last*2;
         
         return answer;
     }
